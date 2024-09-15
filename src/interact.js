4b20e9e708fd3374eb10a16d88f7ef3d1582f8bc
MissLeo8 <naledimasello@gmail.com>
2024-09-15 21:40:15 +0200

    injected more weather data

--- src/interact.js
+++ src/interact.js
@@ -1,10 +1,41 @@
 function updateWeatherInfo(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let dateTimeElement = document.querySelector("#date-time");
+    let date = new Date(response.data.time * 1000);
 
+    cityElement.innerHTML = response.data.city;
+    dateTimeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days =[
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
 let apiKey = "6t5ec092366cb43a3f1d6fe4b5d01o2a";
@@ -16,11 +47,12 @@ axios.get(apiUrl).then(updateWeatherInfo)
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
     searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Knysna")
 
