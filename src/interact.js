7468ddefc43c9a4ba95708da872d0a659f6b6124
MissLeo8 <naledimasello@gmail.com>
2024-09-15 15:39:41 +0200

    added weather response info to get current weather

--- src/interact.js
+++ src/interact.js
@@ -1,8 +1,24 @@
+function updateWeatherInfo(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city) {
+let apiKey = "6t5ec092366cb43a3f1d6fe4b5d01o2a";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeatherInfo)
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
